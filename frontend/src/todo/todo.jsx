75c1f50ad6c6793a0b618e18304fd0f9238fb483
unknown <maiaradepaulait@gmail.com>
2020-10-12 17:35:07 -0300

    Incluind Component Grid for Resposividade

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import PageHeader from '../template/PageHeader'
+import TodoForm from './TodoForm'
+import TodosList from './TodosList'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
+                <TodoForm />
+                <TodosList /> 
             </div>
         )
     }
