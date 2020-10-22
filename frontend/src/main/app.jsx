727c2bfde61262e447626daed9e1e9954bc31a3c
unknown <maiaradepaulait@gmail.com>
2020-10-22 16:47:10 -0300

    whit redux

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,9 +3,8 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import '../template/custom.css'
 
 import React from 'react'
-import Menu from '../template/Menu'
-import Routes from '../main/Routes'
-
+import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
--- frontend/src/template/Menu.jsx
+++ frontend/src/template/Menu.jsx
@@ -5,15 +5,15 @@ export default props => (
         <div className='container'>
             <div className='navbar-header'>
                 <a className='navbar-brand' href='#'>
-                    <i  className='fa fa-calendar-check-o'></i> Todo
-                </a> 
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
+                </a>
             </div>
+
             <div id='navbar' className='navbar-collapse collapse'>
-                <ul className='nav navbar-nav'>
+                <ul className="nav navbar-nav">
                     <li><a href='#/todos'>Tarefas</a></li>
                     <li><a href='#/about'>Sobre</a></li>
                 </ul>
-
             </div>
         </div>
     </nav>
